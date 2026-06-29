e3984f0d60c5ea554a4422a301ea8e1954606c10
losmafia <matheusrocharibeirorr@gmail.com>
2026-06-29 10:25:35 -0300

    Versão Final Entrega do Projeto. - 3.0

--- frontend/services/api.ts
+++ frontend/services/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Cria a instância base apontando para o seu backend
 export const api = axios.create({
-    baseURL: 'http://localhost:3333/api',
+    baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3333/api',
 });
 
 // Interceptador de Requisição (O "Pedágio")
